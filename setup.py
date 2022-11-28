5ee61701105767cf40c1ce38ab0aadac920f8b1e
Steckdose007 <florian@gritsch.eu>
2022-11-28 15:04:35 +0100

    setup.py governs the installation of this package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='snowflake',
+   version='0.0',
+   description='Creates a window in which randomly colored snowflakes are drawn',
+   author='Florian Gritsch',
+   author_email='DSSS@FAU.de',
+   packages=['snowflake'],  # would be the same as name
+   install_requires= None, #external packages acting as dependencies
+)
