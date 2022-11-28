c51cd44b212662486e87a8495e33d1bca53a3ee9
Steckdose007 <florian@gritsch.eu>
2022-11-28 14:40:43 +0100

    modify the “let_it_snow.py” file in such a way that it uses a randomly chosen color for each snowflake.

--- let_it_snow.py
+++ let_it_snow.py
@@ -5,23 +5,27 @@ import numpy as np
 def main(speed=0, bg_color="grey"):
     # create Turtle object
     turtle_screen = turtle.Screen()
+    #change colormode to 0-255
+    turtle_screen.colormode(255)
     myTurtle = turtle.Turtle()
     
     # set speed to 'fastest = 0'
     myTurtle.speed(speed)
     # change background color
     turtle_screen.bgcolor(bg_color)
-  
- 
-    """TODO: define different colors here"""
 
+    NUM_FLAKES = 20
+    # create array with NUM_FLAKES rows and random colors in each row
+    colors = np.random.rand(NUM_FLAKES, 3) * 255
+    colors = colors.astype(int)
 
-    for _ in range(10):
+    for _ in range(NUM_FLAKES):
         # define some params
         size = 18
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
-        """TODO: set snowflake color here (one of the colors defined above)"""
+        #give turtle object color
+        myTurtle.color(colors[_])
 
         # Go to the start position of the snowflake
         myTurtle.penup()
