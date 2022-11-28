83d19eca0920ad8a8a2c9942eeeba58c08e25768
Steckdose007 <florian@gritsch.eu>
2022-11-28 15:21:50 +0100

    tried different name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='snowflake1',
+   name='snowflake',
    version='0.0',
    description='Creates a window in which randomly colored snowflakes are drawn',
    author='Florian Gritsch',
    author_email='DSSS@FAU.de',
-   packages=['snowflake1'],  # would be the same as name
+   packages=['snowflake'],  # would be the same as name
    install_requires= None, #external packages acting as dependencies
 )
