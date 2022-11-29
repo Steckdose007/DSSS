87f01c3438f29337f961c0449061e41e95563081
Steckdose007 <florian@gritsch.eu>
2022-11-29 21:43:57 +0100

    added init

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ from setuptools import find_packages
 setup(
    name='snowflake',
    version='0.0',
-   description='Creates a window in which randomly colored snowflakes are drawn',
    author='Florian Gritsch',
    author_email='DSSS@FAU.de',
-   url='https://github.com/Steckdose007/DSSS',
-   packages=find_packages(),  # would be the same as name
-   install_requires= ['numpy','turtles'], #external packages acting as dependencies
+   packages=find_packages(),
+   install_requires= ['numpy','turtles'],
 )
